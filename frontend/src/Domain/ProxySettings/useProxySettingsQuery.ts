cd2394409c90ca55bee22ba4c57fbbefbbd3b3bb
Romain Dorgueil <romain@makersquad.fr>
2023-12-09 11:36:48 +0100

    feat: formating of proxy settings

--- frontend/src/Domain/ProxySettings/useProxySettingsQuery.ts
+++ frontend/src/Domain/ProxySettings/useProxySettingsQuery.ts
@@ -1,7 +1,11 @@
 import { useQuery } from "react-query"
 import { useApi } from "Domain/Api"
 
+export interface KeyValueSettings {
+  [key: string]: string | KeyValueSettings
+}
+
 export function useProxySettingsQuery() {
   const api = useApi()
-  return useQuery("proxySettings", () => api.fetch("/settings").then((r) => r.json()))
+  return useQuery<KeyValueSettings>("proxySettings", () => api.fetch("/settings").then((r) => r.json()))
 }
--- frontend/src/Pages/ProxySettings/Settings.tsx
+++ frontend/src/Pages/ProxySettings/Settings.tsx
@@ -1,21 +1,56 @@
 import { useProxySettingsQuery } from "Domain/ProxySettings"
 import { useEffect, useState } from "react"
+import { KeyValueSettings } from "Domain/ProxySettings/useProxySettingsQuery.ts"
+
+const SettingsTable = ({ settings }: { settings: KeyValueSettings }) => {
+  return (
+    <table className="min-w-fit divide-y divide-gray-300">
+      <tbody className="divide-y divide-gray-200 bg-white">
+        {Object.entries(settings).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td className="whitespace-nowrap py-2 px-4 text-sm font-medium text-gray-900 min-w-fit align-top">
+                {key}
+              </td>
+              <td className="whitespace-nowrap text-sm text-gray-500">
+                {typeof value === "string" ? value : <SettingsTable settings={value} />}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
 
 const Settings = () => {
   const query = useProxySettingsQuery()
-  const [settings, setSettings] = useState<string | null>(null)
+  const [settings, setSettings] = useState<KeyValueSettings | null>(null)
 
   useEffect(() => {
     if (query.data) {
-      console.log(query.data)
-      setSettings(JSON.stringify(query.data, null, 2))
+      setSettings(query.data)
     }
   }, [query.data])
 
   return (
-    <div>
-      <h1>{settings}</h1>
-    </div>
+    <>
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Settings</h1>
+          <p className="mt-2 text-sm text-gray-700">Dump of actually applied configuration</p>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+              {settings ? <SettingsTable settings={settings} /> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
