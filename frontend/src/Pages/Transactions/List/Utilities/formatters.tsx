8898c7d8021c5721f52fe2c74f70e06a73ec76c2
Romain Dorgueil <romain@makersquad.fr>
2023-12-16 09:44:47 +0100

    feat: cleaning helm chart, requests ratings a bit less severe

--- frontend/src/Pages/Transactions/List/Utilities/formatters.tsx
+++ frontend/src/Pages/Transactions/List/Utilities/formatters.tsx
@@ -95,11 +95,11 @@ export const durationRatingScale: { label: string; threshold?: number; className
   { label: "A++", threshold: 0.025, className: "bg-teal-400" },
   { label: "A+", threshold: 0.05, className: "bg-emerald-400" },
   { label: "A", threshold: 0.1, className: "bg-green-500" },
-  { label: "B", threshold: 0.2, className: "bg-lime-500" },
-  { label: "C", threshold: 0.4, className: "bg-yellow-500" },
-  { label: "D", threshold: 0.8, className: "bg-amber-500" },
-  { label: "E", threshold: 1.6, className: "bg-orange-500" },
-  { label: "F", threshold: 3.2, className: "bg-red-500" },
+  { label: "B", threshold: 0.25, className: "bg-lime-500" },
+  { label: "C", threshold: 0.5, className: "bg-yellow-500" },
+  { label: "D", threshold: 1.0, className: "bg-amber-500" },
+  { label: "E", threshold: 2.0, className: "bg-orange-500" },
+  { label: "F", threshold: 4.0, className: "bg-red-500" },
   { label: "G", className: "bg-red-700" },
 ]
 
