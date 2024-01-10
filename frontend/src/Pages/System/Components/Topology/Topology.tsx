c069d0f575c94d1e583f423f55eb35649693d1e9
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-01-10 10:26:55 +0100

    feat: use Pane in Topology

--- frontend/src/Pages/System/Components/Topology/Topology.tsx
+++ frontend/src/Pages/System/Components/Topology/Topology.tsx
@@ -1,5 +1,3 @@
-import { H2 } from "mkui/Components/Typography"
-
 type endpoint = {
   name: string
   port: number
@@ -9,78 +7,65 @@ type endpoint = {
 
 type Endpoints = endpoint[]
 
-export const Topology = ({
-  endpoints,
-  className,
-  title,
-}: {
-  endpoints?: Endpoints
-  className?: string
-  title?: string
-}) => {
+export const Topology = ({ endpoints }: { endpoints?: Endpoints }) => {
   return (
     endpoints && (
-      <div className={className}>
-        <H2 className="p-2">{title}</H2>
-        <div className="items-center jutify-center self-center">
-          <svg className="container" viewBox={`0 0 650 ${50 + 100 * endpoints.length}`}>
-            <defs>
-              <marker
-                id="arrow"
-                markerWidth="10"
-                markerHeight="10"
-                refX="0"
-                refY="3"
-                orient="auto"
-                markerUnits="strokeWidth"
-              >
-                <path d="M0,0 L0,6 L9,3 z" fill="#000" />
-              </marker>
-            </defs>
-            <rect x="50" y={50 + (100 * (endpoints.length - 1)) / 2} width="100" height="50" fill="lightblue" />
-            <text
-              x="100"
-              y={75 + (100 * (endpoints.length - 1)) / 2}
-              fill="black"
-              textAnchor="middle"
-              dominantBaseline="middle"
-            >
-              Localhost
-            </text>
+      <svg className="container" viewBox={`0 0 650 ${50 + 100 * endpoints.length}`}>
+        <defs>
+          <marker
+            id="arrow"
+            markerWidth="10"
+            markerHeight="10"
+            refX="0"
+            refY="3"
+            orient="auto"
+            markerUnits="strokeWidth"
+          >
+            <path d="M0,0 L0,6 L9,3 z" fill="#000" />
+          </marker>
+        </defs>
+        <rect x="50" y={50 + (100 * (endpoints.length - 1)) / 2} width="100" height="50" fill="lightblue" />
+        <text
+          x="100"
+          y={75 + (100 * (endpoints.length - 1)) / 2}
+          fill="black"
+          textAnchor="middle"
+          dominantBaseline="middle"
+        >
+          Localhost
+        </text>
 
-            <rect x="250" y="50" width="150" height={70 + 100 * (endpoints.length - 1)} fill="lightblue" />
-            <text x="300" y={75 + (100 * (endpoints.length - 1)) / 2} fill="black">
-              HARP
-            </text>
+        <rect x="250" y="50" width="150" height={70 + 100 * (endpoints.length - 1)} fill="lightblue" />
+        <text x="300" y={75 + (100 * (endpoints.length - 1)) / 2} fill="black">
+          HARP
+        </text>
 
+        <line
+          x1="150"
+          y1={75 + (100 * (endpoints.length - 1)) / 2}
+          x2="240"
+          y2={75 + (100 * (endpoints.length - 1)) / 2}
+          stroke="black"
+          markerEnd="url(#arrow)"
+        />
+
+        {endpoints.map((endpoint, index) => (
+          <g key={index}>
+            <rect x="500" y={60 + index * 100} width="100" height="50" fill="lightblue" />
+            <text x="550" y={90 + index * 100} fill="black" textAnchor="middle" dominantBaseline="middle">
+              {endpoint.name}
+            </text>
             <line
-              x1="150"
-              y1={75 + (100 * (endpoints.length - 1)) / 2}
-              x2="240"
-              y2={75 + (100 * (endpoints.length - 1)) / 2}
+              x1="400"
+              y1={85 + index * 100}
+              x2="490"
+              y2={85 + index * 100}
               stroke="black"
               markerEnd="url(#arrow)"
             />
-
-            {endpoints.map((endpoint, index) => (
-              <g key={index}>
-                <rect x="500" y={60 + index * 100} width="100" height="50" fill="lightblue" />
-                <text x="550" y={90 + index * 100} fill="black" textAnchor="middle" dominantBaseline="middle">
-                  {endpoint.name}
-                </text>
-                <line
-                  x1="400"
-                  y1={85 + index * 100}
-                  x2="490"
-                  y2={85 + index * 100}
-                  stroke="black"
-                  markerEnd="url(#arrow)"
-                />
-              </g>
-            ))}
-          </svg>
-        </div>
-      </div>
+          </g>
+        ))}
+      </svg>
     )
   )
 }
