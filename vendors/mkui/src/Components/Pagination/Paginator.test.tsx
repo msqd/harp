e103d9b60447bcc9380c6394d7310b5f4f247c68
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-02-06 16:42:44 +0100

    tests: add unit tests for paginator

---
+++ vendors/mkui/src/Components/Pagination/Paginator.test.tsx
@@ -0,0 +1,48 @@
+import { render, fireEvent } from "@testing-library/react"
+import { Paginator } from "./Paginator"
+
+describe("Paginator", () => {
+  it("renders without crashing", () => {
+    const { getAllByText } = render(<Paginator />)
+    const previousButtons = getAllByText("Previous")
+    const nextButtons = getAllByText("Next")
+    expect(previousButtons.length).toBe(2)
+    expect(nextButtons.length).toBe(2)
+  })
+
+  describe("when Previous button is clicked", () => {
+    it("calls setPage with correct value when previous span clicked", () => {
+      const setPage = jest.fn()
+      const { getByText } = render(<Paginator current={2} setPage={setPage} />)
+
+      fireEvent.click(getByText("Previous", { selector: "span" }))
+      expect(setPage).toHaveBeenCalledWith(1)
+    })
+
+    it("calls setPage with correct value when previous anchor clicked", () => {
+      const setPage = jest.fn()
+      const { getByText } = render(<Paginator current={2} setPage={setPage} />)
+
+      fireEvent.click(getByText("Previous", { selector: "a" }))
+      expect(setPage).toHaveBeenCalledWith(1)
+    })
+  })
+
+  describe("when Next button is clicked", () => {
+    it("calls setPage with correct value when next span clicked", () => {
+      const setPage = jest.fn()
+      const { getByText } = render(<Paginator current={2} setPage={setPage} />)
+
+      fireEvent.click(getByText("Next", { selector: "span" }))
+      expect(setPage).toHaveBeenCalledWith(3)
+    })
+
+    it("calls setPage with correct value when next anchor clicked", () => {
+      const setPage = jest.fn()
+      const { getByText } = render(<Paginator current={2} setPage={setPage} />)
+
+      fireEvent.click(getByText("Next", { selector: "a" }))
+      expect(setPage).toHaveBeenCalledWith(3)
+    })
+  })
+})
--- vendors/mkui/src/Components/Pagination/Paginator.tsx
+++ vendors/mkui/src/Components/Pagination/Paginator.tsx
@@ -69,10 +69,13 @@ export function Paginator({
             {/* Current: "z-10 bg-primary-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600",
                 Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
 
-            {generate(current, pages, 1, 1, "…").map((i) => {
+            {generate(current, pages, 1, 1, "…").map((i, index) => {
               if (typeof i === "string") {
                 return (
-                  <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+                  <span
+                    key={index}
+                    className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
+                  >
                     ...
                   </span>
                 )
@@ -80,6 +83,7 @@ export function Paginator({
                 const isCurrent = i === current
                 return (
                   <a
+                    key={index}
                     href="#"
                     aria-current={isCurrent ? "page" : undefined}
                     className={
