f8bb537267a71cec6e023c3aaa93142c11b7de04
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-02-09 16:25:00 +0100

    test: response heading test

---
+++ frontend/src/Pages/Transactions/Components/Elements/ResponseHeading.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+
+import { ResponseHeading } from "./ResponseHeading"
+
+describe("ResponseHeading", () => {
+  test("renders correctly with response", () => {
+    const response = {
+      summary: "HTTP/1.1 200",
+      transaction_id: "1",
+      kind: "response",
+      headers: "header1:value1\nheader2:value2",
+      body: "Foo",
+    }
+
+    render(<ResponseHeading response={response} />)
+
+    // Check that the status code is rendered
+    expect(screen.getByText("200 OK")).toBeInTheDocument()
+  })
+})
