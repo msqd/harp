9df3641bec5236d45ca84763f854a6e1544cf7cc
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-02-12 10:22:22 +0100

    test: remove console logs from page test

--- frontend/src/Components/Page/Page.test.tsx
+++ frontend/src/Components/Page/Page.test.tsx
@@ -1,10 +1,14 @@
 import { render, screen } from "@testing-library/react"
 import { ErrorBoundary } from "react-error-boundary"
-import { describe, test, expect } from "vitest"
+import { describe, test, expect, vi, afterEach } from "vitest"
 
 import { Page, Error as ErrorFunc } from "./Page"
 
 describe("Page", () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   test("renders without error", () => {
     const { container } = render(<Page children={<div />} title="Test Title" description="Test Description" />)
     expect(container).toMatchSnapshot()
@@ -17,6 +21,7 @@ describe("Page", () => {
   })
 
   test("renders Error component when child throws error", () => {
+    vi.spyOn(console, "error").mockImplementation(() => {})
     const Child = () => {
       throw new Error("Test error")
     }
