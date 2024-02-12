b58cdcd662d82052175d20e1c899d8946d9af7ca
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-02-12 19:56:52 +0100

    test: test systemdependenciestabpanel

--- frontend/src/Pages/System/SystemDependenciesTabPanel.test.tsx
+++ frontend/src/Pages/System/SystemDependenciesTabPanel.test.tsx
@@ -1,10 +1,19 @@
+import { ErrorBoundary } from "react-error-boundary"
 import { renderWithClient } from "tests/utils"
 import { expect, it } from "vitest"
 
-import { SystemDependenciesTabPanel } from "./SystemDependenciesTabPanel"
+import { Error } from "Components/Page"
+import { Tab } from "mkui/Components/Tabs"
 
+import { SystemDependenciesTabPanel } from "./SystemDependenciesTabPanel"
 it("renders the title and data when the query is successful", async () => {
-  const result = renderWithClient(<SystemDependenciesTabPanel />)
+  const result = renderWithClient(
+    <ErrorBoundary FallbackComponent={Error}>
+      <Tab.Group>
+        <SystemDependenciesTabPanel />
+      </Tab.Group>
+    </ErrorBoundary>,
+  )
 
   await result.findByText("numpy")
   expect(result.container).toMatchSnapshot()
