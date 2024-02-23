732d2fde445150c31e40a6b6ba8edec00a5995c7
Romain Dorgueil <romain@makersquad.fr>
2024-02-23 08:21:17 +0100

    feat: small cli doc

--- harp/commandline/__init__.py
+++ harp/commandline/__init__.py
@@ -15,6 +15,19 @@ dev).")
 Manage process list / options using a class (with hierarchy for prod/dev, for example)
 If one process only, no honcho ? Or mayne no honcho manager ?
 
+
+The `entrypoint` callable is the entrypoint for the `harp` command line interface, installed into your environment when
+running either `poetry install`or `pip install`.
+
+It must be considered as a high-level development helper: for example, `harp start` wraps `honcho`, a python
+process manager (foreman clone) that can spawn multiple processes to help you with development.
+
+Production environment will favour the lower-level `bin/entrypoint` script, that skips the process manager entirely
+and runs the application server directly.
+
+This may be subject to changes in the future (espacially to align/refactor arg parsers), but this approach works
+well for now.
+
 """
 
 import rich_click as click
@@ -25,6 +38,11 @@ from harp.commandline.start import start
 
 @click.group()
 def entrypoint():
+    """HTTP Application Runtime Proxy (HARP)
+
+    The following commands are available to help you setup and run your HTTP proxy application.
+
+    """
     pass
 
 
