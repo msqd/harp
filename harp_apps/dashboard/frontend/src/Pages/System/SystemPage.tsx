998b34cf7957ff49fff1f7255fb3b831d8120c22
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-03-29 15:05:45 +0100

    hide topology tab in system

--- harp_apps/dashboard/frontend/src/Pages/System/SystemPage.tsx
+++ harp_apps/dashboard/frontend/src/Pages/System/SystemPage.tsx
@@ -1,5 +1,6 @@
-import { Page, PageTitle } from "Components/Page"
 import { Tab } from "ui/Components/Tabs"
+
+import { Page, PageTitle } from "Components/Page"
 
 import { SystemDependenciesTabPanel } from "./SystemDependenciesTabPanel.tsx"
 import { SystemSettingsTabPanel } from "./SystemSettingsTabPanel.tsx"
@@ -11,13 +12,11 @@ export const SystemPage = () => {
     <Page title={<PageTitle title="System" description="Informations about the running instance." />}>
       <Tab.Group>
         <Tab.List as="nav" aria-label="Tabs">
-          <Tab>Topology</Tab>
           <Tab>Settings</Tab>
           <Tab>Storage</Tab>
           <Tab>Dependencies</Tab>
         </Tab.List>
         <Tab.Panels>
-          <SystemTopologyTabPanel />
           <SystemSettingsTabPanel />
           <SystemStorageTabPanel />
           <SystemDependenciesTabPanel />
