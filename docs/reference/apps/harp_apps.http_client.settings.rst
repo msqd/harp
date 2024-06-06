f6b7b5e497440442f568e4c8ee18089924dfac52
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-06-06 10:55:57 +0200

    feat: configurable timeout and cache

---
+++ docs/reference/apps/harp_apps.http_client.settings.rst
@@ -0,0 +1,7 @@
+harp_apps.http_client.settings
+==============================
+
+.. automodule:: harp_apps.http_client.settings
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- harp_apps/http_client/__app__.py
+++ harp_apps/http_client/__app__.py
@@ -1,20 +1,43 @@
-from hishel import AsyncCacheTransport
+from hishel import HEURISTICALLY_CACHEABLE_STATUS_CODES, AsyncCacheTransport, Controller
 from httpx import AsyncClient, AsyncHTTPTransport
 
+from harp import get_logger
 from harp.config import Application
 from harp.config.events import FactoryBindEvent
 from harp.settings import DEFAULT_TIMEOUT
 
+from .settings import HttpClientSettings
+
+logger = get_logger(__name__)
+
+
+class AsyncHttpClient(AsyncClient):
+    settings: HttpClientSettings
+
+    def __init__(self, settings: HttpClientSettings):
+        transport = AsyncHTTPTransport()
+        if settings.cache and not settings.cache.disabled:
+            controller = Controller(
+                allow_heuristics=True,
+                cacheable_methods=settings.cache.cacheable_methods,
+                cacheable_status_codes=settings.cache.cacheable_status_codes
+                or list(HEURISTICALLY_CACHEABLE_STATUS_CODES),
+                allow_stale=True,
+            )
+            transport = AsyncCacheTransport(transport=transport, controller=controller)
+
+        super().__init__(transport=transport, timeout=settings.timeout)
+
 
 class HttpClientApplication(Application):
+    settings_namespace = "http_client"
+    settings_type = HttpClientSettings
+
+    @classmethod
+    def defaults(cls, settings=None) -> dict:
+        settings = settings if settings is not None else {}
+        settings.setdefault("timeout", DEFAULT_TIMEOUT)
+        return settings
+
     async def on_bind(self, event: FactoryBindEvent):
-        # todo timeout config ?
-        # todo lazy build ?
-        transport = AsyncHTTPTransport()
-        cache_transport = AsyncCacheTransport(transport=transport)
-        event.container.add_instance(
-            AsyncClient(
-                timeout=DEFAULT_TIMEOUT,
-                transport=cache_transport,
-            )
-        )
+        event.container.add_singleton(AsyncClient, AsyncHttpClient)
