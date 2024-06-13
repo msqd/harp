658c38ab0af1506120abe78de4a87e7fe1daae39
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-06-13 20:10:33 +0200

    test: tests for facets

---
+++ harp_apps/dashboard/tests/filters/test_base.py
@@ -0,0 +1,42 @@
+from multidict import MultiDict
+
+from harp_apps.dashboard.filters.base import AbstractMinMaxFacet, NonExhaustiveFacet
+
+
+def test_non_exhaustive_facet():
+    facet = NonExhaustiveFacet()
+    assert "NULL" in facet.choices
+
+
+class TestAbstractMinMaxFacet:
+    facet = AbstractMinMaxFacet()
+
+    def test_values(self):
+        self.facet.meta = {"min": 10, "max": 90}
+        assert self.facet.values == {"min": 10, "max": 90}
+
+    def test_filter(self):
+        result = self.facet.filter(10, 90)
+        assert result == {"values": ["min", "max"], "current": {"min": 10, "max": 90}}
+
+    def test_filter_from_query(self):
+        self.facet.name = "test"
+        query = MultiDict({"testmin": "10", "testmax": "90"})
+        result = self.facet.filter_from_query(query)
+        assert result == {"values": ["min", "max"], "current": {"min": 10.0, "max": 90.0}}
+
+    def test_get_filter(self):
+        result = self.facet.get_filter(10, 90)
+        assert result == {"min": 10, "max": 90}
+
+    def test_get_filter_from_query(self):
+        self.facet.name = "test"
+        query = MultiDict({"testmin": "10", "testmax": "90"})
+        result = self.facet.get_filter_from_query(query)
+        assert result == {"min": 10.0, "max": 90.0}
+
+    def test_min_max_from_query(self):
+        self.facet.name = "test"
+        query = MultiDict({"testmin": "10", "testmax": "90"})
+        result = self.facet._min_max_from_query(query)
+        assert result == (10.0, 90.0)
