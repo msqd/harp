451129463e09a19b257681a96d0dc4d1442b396b
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-06-14 15:16:29 +0200

    feat: pages meta via helmets

--- harp_apps/dashboard/frontend/src/Pages/Overview/OverviewPage.tsx
+++ harp_apps/dashboard/frontend/src/Pages/Overview/OverviewPage.tsx
@@ -1,11 +1,16 @@
+import { Helmet } from "react-helmet"
+
 import { Page, PageTitle } from "Components/Page"
 
 import { HistorySection } from "./Sections/HistorySection.tsx"
 import { SummarySection } from "./Sections/SummarySection.tsx"
-
 export const OverviewPage = () => {
   return (
     <Page title={<PageTitle title="Overview" />}>
+      <Helmet>
+        <title>Harp UI</title>
+        <meta name="description" content="System page" />
+      </Helmet>
       <SummarySection />
       <HistorySection />
     </Page>
