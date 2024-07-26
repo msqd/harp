1967be2985ea122111d2af4bef48c78cf5521c63
Romain Dorgueil <romain@makersquad.fr>
2024-07-26 19:20:46 +0200

    feat: normalize urls before setting up remotes

--- harp/utils/tests/test_urls.py
+++ harp/utils/tests/test_urls.py
@@ -1,6 +1,13 @@
-from harp.utils.urls import extract_hostname_from_url
+from ..urls import extract_hostname_from_url, normalize_url
 
 
 def test_extract_hostname_from_url():
     assert extract_hostname_from_url("https://www.google.com/") == "www.google.com"
     assert extract_hostname_from_url("https://example.com:1234/") == "example.com"
+
+
+def test_normalize_url():
+    assert normalize_url("https://www.google.com/") == "https://www.google.com/"
+    assert normalize_url("https://www.google.com") == "https://www.google.com/"
+    assert normalize_url("Https://WWW.GOOGLE.COM") == "https://www.google.com/"
+    assert normalize_url("http://example.com/foo/bar/baz") == "http://example.com/foo/bar/baz"
--- harp_apps/proxy/models/remotes.py
+++ harp_apps/proxy/models/remotes.py
@@ -2,6 +2,8 @@ from collections import deque
 from dataclasses import dataclass, field
 from typing import Iterable
 
+from harp.utils.urls import normalize_url
+
 CHECKING = 0
 UP = 1
 DOWN = -1
@@ -21,20 +23,24 @@ class Remote:
     name = None
     url_statuses = None
     current_pool = None
+    mode = None
 
-    def __init__(self, name, /, *, base_urls: Iterable, fallback_urls: Iterable = (), min_pool_size=1):
+    def __init__(self, name, /, *, base_urls: Iterable = (), fallback_urls: Iterable = (), min_pool_size=1):
         self.name = name
+        self.mode = DEFAULT_POOL
 
         self.url_statuses = {}
         for url in base_urls:
+            url = normalize_url(url)
             self.url_statuses.setdefault(url, Url(url=url))
             self.url_statuses[url].pools.add(DEFAULT_POOL)
 
         for url in fallback_urls:
+            url = normalize_url(url)
             self.url_statuses.setdefault(url, Url(url=url))
             self.url_statuses[url].pools.add(FALLBACK_POOL)
 
-        self.min_pool_size = min_pool_size
+        self.min_pool_size = max(0, min_pool_size)
         self.current_pool = deque()
         self.refresh()
 
@@ -47,22 +53,30 @@ class Remote:
         if len(refreshed) < self.min_pool_size:
             for url in self.url_statuses.values():
                 if FALLBACK_POOL in url.pools and url.status >= CHECKING:
+                    self.mode = FALLBACK_POOL
                     refreshed.append(url)
+        else:
+            self.mode = DEFAULT_POOL
 
         self.current_pool = refreshed
 
     def get_url(self):
         try:
             return self.current_pool[0].url
+        except IndexError as exc:
+            raise IndexError(f"No available URLs for remote «{self.name}».") from exc
+
         finally:
             self.current_pool.rotate(-1)
 
     def set_down(self, url):
+        url = normalize_url(url)
         url = self.url_statuses[url]
         url.status = DOWN
         self.refresh()
 
     def set_up(self, url):
+        url = normalize_url(url)
         url = self.url_statuses[url]
         url.status = UP
         self.refresh()
