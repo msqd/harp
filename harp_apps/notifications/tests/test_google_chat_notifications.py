c08e11e45b4dd7e16ddd1f90f70227c23ebe5d8d
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-08-07 12:11:24 +0200

    tests: basic notifications tests for each sender

--- harp_apps/notifications/tests/test_google_chat_notifications.py
+++ harp_apps/notifications/tests/test_google_chat_notifications.py
@@ -1,6 +1,9 @@
+from unittest import mock
+
 import pytest
 
 from harp_apps.notifications.senders import GoogleChatNotificationSender
+from harp_apps.notifications.senders.google_chat import AsyncClient
 
 
 @pytest.mark.parametrize(
@@ -27,3 +30,22 @@ def test_message_does_not_contain_link_if_no_public_url(public_url, expected_lin
     link_button = any(widget_contains_buttons(section) for section in sections)
 
     assert link_button == expected_link
+
+
+async def test_send_notification():
+    with mock.patch.object(AsyncClient, "post", new_callable=mock.AsyncMock) as mock_post:
+        webhook_url = "http://example.com/webhook"
+        public_url = "http://example.com"
+        sender = GoogleChatNotificationSender(webhook_url, public_url)
+
+        mock_post.return_value.status_code = 200
+
+        method = "POST"
+        url = "http://example.com/api"
+        status_code = 500
+        message = "Internal Server Error"
+        transaction_id = "12345"
+
+        await sender.send_notification(method, url, status_code, message, transaction_id)
+        mock_post.assert_called_once()
+        assert mock_post.call_args[0][0] == webhook_url
--- harp_apps/notifications/tests/test_slack_notifications.py
+++ harp_apps/notifications/tests/test_slack_notifications.py
@@ -1,6 +1,9 @@
+from unittest import mock
+
 import pytest
 
 from harp_apps.notifications.senders import SlackNotificationSender
+from harp_apps.notifications.senders.slack import AsyncWebhookClient
 
 
 @pytest.mark.parametrize(
@@ -23,3 +26,23 @@ def test_message_does_not_contain_link_if_no_public_url(public_url, expected_lin
     link_button = any(v == "actions" for block in blocks for k, v in block.items())
 
     assert link_button == expected_link
+
+
+async def test_send_notification():
+    with mock.patch.object(AsyncWebhookClient, "send", new_callable=mock.AsyncMock) as mock_send:
+        webhook_url = "http://example.com/webhook"
+        public_url = "http://example.com"
+        sender = SlackNotificationSender(webhook_url, public_url)
+
+        mock_send.return_value.status_code = 200
+        mock_send.return_value.body = "ok"
+
+        method = "POST"
+        url = "http://example.com/api"
+        status_code = 500
+        message = "Internal Server Error"
+        transaction_id = "12345"
+
+        await sender.send_notification(method, url, status_code, message, transaction_id)
+        mock_send.assert_called_once()
+        assert mock_send.call_args[1]["text"] == "ERROR NOTIFICATION"
