b2b3407bf0c8d9380d3aebe9b4080dcc74bf4e40
Arthur Degonde <44548105+ArthurD1@users.noreply.github.com>
2024-11-12 17:17:04 +0100

    refactor: change controller service definition variable name

--- harp_apps/proxy/__app__.py
+++ harp_apps/proxy/__app__.py
@@ -38,8 +38,7 @@ async def on_bind(event: OnBindEvent):
         if endpoint.controller is not None:
             controller = endpoint.controller
             name = f"proxy.controllers.{endpoint.name}_controller"
-            ControllerServiceDefinition = controller.to_service_definition(name, lifestyle="transient")
-            resolver = ServiceResolver(event.container, ControllerServiceDefinition)
+            resolver = ServiceResolver(event.container, controller.to_service_definition(name, lifestyle="transient"))
             event.container._map[name] = resolver
     event.container.add_singleton(Proxy, cast(type, ProxyFactory))
 
